0130a734947e9f1e3d1ef6e1ef1f38b529a9cb79
Rosario Nici <rosarionici95@gmail.com>
2022-11-10 08:37:13 +0100

    create table of trians end

--- database/migrations/2022_11_05_145333_create_trains_table.php
+++ database/migrations/2022_11_05_145333_create_trains_table.php
@@ -6,26 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateTrainsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('trains', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('trains');
+    public function up()
+        {
+            Schema::create('trains', function (Blueprint $table) {
+                $table->id();
+                $table->string('azienda', 100); // tabella aziende?
+                $table->string('stazione_partenza', 50); // tabella stazione?
+                $table->string('stazione_arrivo', 50); // tabella stazione?
+                $table->time('orario_di_partenza');
+                $table->time('orario_di_arrivo');
+                $table->date('data_di_partenza');
+                $table->date('data_di_arrivo');
+                $table->string('codice_treno', 10)->unique();
+                $table->tinyInteger('numero_carrozze')->unsigned()->default(0);
+                $table->boolean('in_orario')->default(true);
+                $table->tinyInteger('cancellato')->default(0);
+
+            });
+        }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema::dropIfExists('trains');
+        }
     }
-}
